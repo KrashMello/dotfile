fa3406a0998e7fb06865f62de4f14a72cec4f5a2
KrashMello <joelenrikesoteldo@gmail.com>
2024-05-02 06:04:14 -0400

    feat: nvim nordic theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,9 +6,17 @@ return {
     "arcticicestudio/nord-vim",
   },
   {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("nordic").load()
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = "nordic",
     },
   },
 }
