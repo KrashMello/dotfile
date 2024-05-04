3e4219004cbda46d18970a9183c9124db9e9cacb
KrashMello <joelenrikesoteldo@gmail.com>
2024-05-04 10:39:55 -0400

    feac: atac add to ui nvim

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -82,6 +82,16 @@ return {
       "nvim-lua/plenary.nvim",
     },
   },
+  -- atac api
+  {
+    "NachoNievaG/atac.nvim",
+    dependencies = { "akinsho/toggleterm.nvim" },
+    config = function()
+      require("atac").setup({
+        dir = os.getenv("PWD") .. "/base-collections", -- By default, the dir will be set as /tmp/atac
+      })
+    end,
+  },
   -- statusline
   -- {
   --   "nvim-lualine/lualine.nvim",
