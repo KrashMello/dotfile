e6384b7135efef1b1a3ee59c5f597314f3b896ce
KrashMello <joelenrikesoteldo@gmail.com>
2024-05-04 10:40:53 -0400

    feat: comment nord themes and add base16-nord nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,21 +2,21 @@ return {
   { "rose-pine/neovim", name = "rose-pine", opts = {
     variant = "moon",
   } },
-  {
-    "arcticicestudio/nord-vim",
-  },
-  {
-    "AlexvZyl/nordic.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("nordic").load()
-    end,
-  },
+  -- {
+  --   "shaunsingh/nord.nvim",
+  -- },
+  -- {
+  --   "AlexvZyl/nordic.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require("nordic").load()
+  --   end,
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nordic",
+      colorscheme = "base16-nord",
     },
   },
 }
