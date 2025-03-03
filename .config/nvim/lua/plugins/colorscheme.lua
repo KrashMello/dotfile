84b77aa4238d560345834634988625c11e06aa91
krashmello <joelenrikesoteldo@gmail.com>
2025-03-03 11:30:55 -0400

    feat(nvim): change colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -13,8 +13,8 @@ return {
         -- or refer to the configuration section
         -- for configuration options
         bold_vert_split = false, -- use bold vertical separators
-        dim_nc_background = false, -- dim 'non-current' window backgrounds
-        disable_background = false, -- disable background
+        dim_nc_background = true, -- dim 'non-current' window backgrounds
+        disable_background = true, -- disable background
         disable_float_background = false, -- disable background for floats
         disable_italics = false, -- disable italics
       })
@@ -82,21 +82,10 @@ return {
       })
     end,
   }, -- and this
-  -- {
-  "shaunsingh/nord.nvim",
-  -- },
-  -- {
-  --   "AlexvZyl/nordic.nvim",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require("nordic").load()
-  --   end,
-  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-frappe",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
