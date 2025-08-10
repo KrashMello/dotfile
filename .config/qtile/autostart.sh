f0b5db1c6f5952e2a4c6adfb7497bd4bc87be8e9
KrashMello <joelenrikesoteldo@gmail.com>
2025-08-10 15:39:06 -0400

    feat(qtile): fix script autostart

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 #
-function run {
-  if ! pgrep -x $(basename $1 | head -c 15) 1>/dev/null;
-  then
-    $@&
+run() {
+  if ! pgrep -x "$(basename "$1" | head -c 15)" 1>/dev/null; then
+    "$@" &
   fi
 }
 PATH="$HOME/.config/qtile/scripts:$PATH"
 # dimension wide
 xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x85 --rotate normal --output HDMI-2 --off --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-3 --off
 picom &
-feh --bg-fill $HOME/Imágenes/background/km_background.png
+feh --bg-fill "$HOME/Imágenes/background/km_background.png"
 # start all this to entry the system
-sxhkd -c $HOME/.config/qtile/sxhkdrc &
-dunst -config "$HOME"/.config/qtile/dunstrc &
+sxhkd -c "$HOME/.config/qtile/sxhkdrc" &
+dunst -config "$HOME/.config/qtile/dunstrc" &
 # xss-lock -- i3lockmore -n --image-fill $HOME/Imágenes/background/surreal_art_by_thenumberoneai_dgfi3bl-fullview.jpg &
 run variety &
 run nm-applet &
@@ -23,4 +22,3 @@ udiskie -t &
 blueman-applet &
 kitty &
 clipcatd &
-
