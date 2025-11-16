cf6a6b224d7cc4d5a99e041adaed64ed581a85c6
KrashMello <joelenrikesoteldo@gmail.com>
2025-11-16 13:52:08 -0400

    feat(qtile): update auto start script

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -5,16 +5,23 @@ run() {
     "$@" &
   fi
 }
-PATH="$HOME/.config/qtile/scripts:$PATH"
+
+#scripts
+if [ -d "$HOME/.config/qtile/scripts/" ]; then
+  PATH="$HOME/.config/qtile/scripts/:$PATH"
+fi
+
 # dimension wide
-xrandr --output HDMI-1 --mode 1920x1080 --pos 1920x85 --rotate normal --output HDMI-2 --off --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-3 --off
+xrandr --output HDMI-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output HDMI-2 --off --output DP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-3 --off
 picom &
-feh --bg-fill "$HOME/Imágenes/background/km_background.png"
+if [ -f "$HOME/.fehbg" ]; then
+  "$HOME/.fehbg"
+fi
 # start all this to entry the system
 sxhkd -c "$HOME/.config/qtile/sxhkdrc" &
 dunst -config "$HOME/.config/qtile/dunstrc" &
 # xss-lock -- i3lockmore -n --image-fill $HOME/Imágenes/background/surreal_art_by_thenumberoneai_dgfi3bl-fullview.jpg &
-run variety &
+# run variety &
 run nm-applet &
 run blueman-applet &
 numlockx on &
